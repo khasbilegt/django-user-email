7da18b0c9ee078ae5d52108e8f40943a4c7c3417
khasbilegt <khasbilegt.ts@gmail.com>
2023-12-21 16:53:07 +0800

    session install django

--- noxfile.py
+++ noxfile.py
@@ -6,13 +6,16 @@ import nox
 @nox.session
 @nox.parametrize("django", ["3.2", "4.0", "4.2", "5.0", "main"])
 def test(session, django):
-    if django == "main" and not sys.version_info.minor >= 8:
+    if django == "main" and not sys.version_info.minor >= 10:
         session.skip()
     else:
         if django == "main":
-            session.install("https://github.com/django/django/archive/main.tar.gz")
+            session.run(
+                "pip", "install", "https://github.com/django/django/archive/main.tar.gz"
+            )
         else:
-            session.install(f"django=={django}")
+            session.run("pip", "install", f"django=={django}")
+        session.run("python", "install")
         session.run("python", "makemigrations.py")
         session.run("coverage", "run", "runtests.py")
         session.run("coverage", "report")
